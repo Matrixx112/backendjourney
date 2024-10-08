5d42b3e33ba7772e219c01dcfd00c56ec9a5bd18
Matrixx112 <chiemekaonwunkwe@gmail.com>
2024-10-09 05:45:49 +0800

    my good project

---
+++ class1.py
@@ -0,0 +1,46 @@
+eng = 100 #this is great
+math=50 #this is average
+chem=65 #this is average
+dp=90 #this is great
+bio=30 #this is bad
+phy=10 #this is terrible
+math = 50 #this is average
+my_email="chikaso111@gmail.com" #this is email address
+my_friend="emeka" #this is friend's name
+
+# sum = eng + math
+
+# print(sum)
+sub= chem - bio
+print(sub)
+div = dp * phy
+print(div)
+mod = chem % math
+print(mod)
+
+print(my_email)
+speed=1.5
+print(speed)
+s=" and favour"
+print(my_friend + s)
+print("emeka" + s)
+
+s.upper
+print(s.replace("u","f"))
+print(my_email.upper())
+print(s.__len__())
+
+# name=input("what is your name?")
+# print("your name is " + name)
+
+#simple calculator to get addition and subtraction
+numb1 = int(input("any number"))
+numb2 = int (input("another number"))
+w = numb1 + numb2
+m = numb1 - numb2
+
+print(f"the sum of the number is {w}")
+print(f"the subtraction of the number is {m}")
+
+from math import *
+print(sqrt(100))
