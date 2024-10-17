41c0b599311c041cac3c316ae33a60b2a3f99360
Matrixx112 <chiemekaonwunkwe@gmail.com>
2024-10-17 14:57:31 +0800

    assignment

---
+++ assignment1.py
@@ -0,0 +1,41 @@
+# odd or even number checker
+x=int(input("enter any number"))
+if (x % 2) == 0:
+    print("number is even")
+else:
+    print("number is odd")
+
+
+# to check grades
+g=int(input("enter grade"))
+# The marks must be between the range of 0-100
+if (g <= 20):
+        print("F")
+elif (g >= 21) and (g <= 39):
+        print("E")
+elif (g >= 40) and (g <= 49):
+        print("D")
+elif (g >= 50) and (g <= 64):
+        print("C")
+elif (g >= 65) and (g <= 74):
+        print("B")
+elif (g >= 75) and (g <= 100):
+        print("A")
+
+
+# to check positive,negative or zero numbers
+y=(int(input("enter any number")))
+if (y == 0):
+    print("zero")
+if (y > 0):
+    print("positive")
+if (y < 0):
+    print("negative")
+        
+u=(int(input("enter year")))
+if (u % 4 ==0 and u % 100 != 0) or (u % 400 == 0):
+    print(f"{u} is a leap year")
+else:
+    print(f"{u} is not a leap year")
+ 
+
